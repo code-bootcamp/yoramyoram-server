28d2b2c2fafcd909137cb13b0bf7570ed10ea4fa
장시혁 <jangsihyeog@jangsihyeog-ui-MacBookPro.local>
2022-12-26 22:11:13 +0900

    [ feat ] login

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './apis/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
