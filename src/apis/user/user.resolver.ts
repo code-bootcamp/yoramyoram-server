5562cacbfca80fdf219817b73497b0f3f8b9adbb
gyungwan <zddx1994@gmail.com>
2022-12-27 13:54:52 +0900

    [ feat ] 유저아이디 찾기기능 생성

--- src/apis/user/user.resolver.ts
+++ src/apis/user/user.resolver.ts
@@ -1,4 +1,6 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInpput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
 
 import { UsersService } from './user.service';
 
@@ -9,10 +11,20 @@ export class UsersResolver {
     private readonly userSerivice: UsersService,
   ) {}
 
-  @Mutation()
-  async createUser({ CreateUserInpput }) {
-    return this.userSerivice.create({
-      CreateUserInpput,
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserInpput') createUserInpput: CreateUserInpput,
+  ) {
+    return await this.userSerivice.create({
+      createUserInpput,
     });
+  }
+
+  @Query(() => String)
+  async findUserEmail(
+    @Args('name') name: string, //
+    @Args('phone') phone: string,
+  ) {
+    return await this.userSerivice.findOne({ name, phone });
   }
 }
