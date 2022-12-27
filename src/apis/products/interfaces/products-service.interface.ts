6ea40fa22666af640707fd908dbc7c019370d84b
soraji <wlthfk0211@gmail.com>
2022-12-27 15:01:29 +0900

    [ feature ] 상품조회fetchProduct  api 생성

--- src/apis/products/interfaces/products-service.interface.ts
+++ src/apis/products/interfaces/products-service.interface.ts
@@ -3,3 +3,7 @@ import { CreateProductInput } from '../dto/create-product.input';
 export interface IProductsServiceCreate {
   createProductInput: CreateProductInput;
 }
+
+export interface IProductsServiceFindOne {
+  productId: string;
+}
--- src/apis/products/product.resolver.ts
+++ src/apis/products/product.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { CreateProductInput } from './dto/create-product.input';
 import { Product } from './entities/product.entity';
 import { ProductsService } from './product.service';
@@ -7,6 +7,18 @@ import { ProductsService } from './product.service';
 export class ProductsResolver {
   constructor(private readonly productsService: ProductsService) {}
 
+  //-------------------------*조회*----------------------------//
+  @Query(() => [Product])
+  fetchProducts(): Promise<Product[]> {
+    return this.productsService.findAll();
+  }
+  //조회(한개)
+  @Query(() => Product)
+  fetchProduct(@Args('productId') productId: string): Promise<Product> {
+    return this.productsService.findOne({ productId });
+  }
+
+  //-------------------------*생성*----------------------------//
   @Mutation(() => Product)
   createProduct(
     @Args('createProductInput') createProductInput: CreateProductInput,
