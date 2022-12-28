d09c093ac2b457d35387303cdeb1db69379ec76c
Potatodori <jshj5181@naver.com>
2022-12-28 19:46:21 +0900

    [ feature ] Logout 기능추가

--- src/commons/auth/jwt-access.strategy.ts
+++ src/commons/auth/jwt-access.strategy.ts
@@ -1,19 +1,33 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER, Inject, UnauthorizedException } from '@nestjs/common';
 
 export class JwtAccessStrategy extends PassportStrategy(Strategy, 'access') {
-  constructor() {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_ACCESS_KEY,
+      passReqToCallback: true,
     });
   }
 
-  validate(payload) {
-    console.log(payload);
+  async validate(req, payload) {
+    const access_token = req.headers.authorization.replace('Bearer ', '');
+
+    const result = await this.cacheManager.get(`access_token:${access_token}`);
+    console.log(result);
+    if (result === 'accessToken') {
+      throw new UnauthorizedException('이미 로그아웃된 토큰입니다.');
+    }
+
     return {
       email: payload.email,
       id: payload.sub,
+      exp: payload.exp,
     };
   }
 }
--- src/commons/auth/jwt-refresh.strategy.ts
+++ src/commons/auth/jwt-refresh.strategy.ts
@@ -1,8 +1,13 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER, Inject, UnauthorizedException } from '@nestjs/common';
 
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
-  constructor() {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {
     super({
       jwtFromRequest: (req) => {
         console.log(req);
@@ -11,14 +16,25 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
         return refreshToken;
       },
       secretOrKey: process.env.JWT_REFRESH_KEY,
+      passReqToCallback: true,
     });
   }
 
-  validate(payload) {
-    console.log(payload);
+  async validate(req, payload) {
+    // console.log(payload);
+    const refresh_token = req.headers.cookie.replace('refreshToken=', '');
+
+    const result = await this.cacheManager.get(
+      `refresh_token:${refresh_token}`,
+    );
+    if (result) {
+      throw new UnauthorizedException('로그아웃된 토큰입니다.');
+    }
+
     return {
       email: payload.email,
       id: payload.sub,
+      exp: payload.exp,
     };
   }
 }
