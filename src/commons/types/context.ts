e3cb44001a526bd6c00ab7aca32edefb779ac15a
Potatodori <jshj5181@naver.com>
2022-12-28 10:17:36 +0900

    [ feature ] Login 기능 RefreshToken 추가

---
+++ src/commons/types/context.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+
+export interface IAuthUser {
+  user?: {
+    email: string;
+    id: string;
+  };
+}
+
+export interface IContext {
+  req: Request & IAuthUser;
+  res: Response;
+}
