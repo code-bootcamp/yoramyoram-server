d9983d630a9b77a5aa9577d7d622c8f137357095
soraji <wlthfk0211@gmail.com>
2023-01-06 16:17:27 +0900

    [ modify ] accessToken 유효시간 늘리기

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -44,7 +44,7 @@ export class AuthService {
   getAccessToken({ user }: IAuthServiceGetAccessToken): string {
     return this.jwtService.sign(
       { email: user.email, sub: user.id },
-      { secret: process.env.JWT_ACCESS_KEY, expiresIn: '1m' },
+      { secret: process.env.JWT_ACCESS_KEY, expiresIn: '1h' },
     );
   }
 }
