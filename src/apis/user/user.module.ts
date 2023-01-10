a80201a5722d052dd3c6e64634b0a78f73b16439
soraji <wlthfk0211@gmail.com>
2023-01-10 22:59:33 +0900

    [ modify ] comment repository 추가

--- src/apis/user/user.module.ts
+++ src/apis/user/user.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtAccessStrategy } from 'src/commons/auth/jwt-access.strategy';
-
+import { Comment } from '../comments/entities/comment.entity';
+import { PhoneService } from '../phone/phone.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './user.resolver';
 import { UsersService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Comment])],
   providers: [
     JwtAccessStrategy,
     UsersResolver, //
     UsersService,
+    PhoneService,
   ],
 })
 export class UsersModule {}
