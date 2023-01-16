28c347ffc88f8444fb46da25c3daa9c45a03d46e
soraji <wlthfk0211@gmail.com>
2023-01-16 18:34:07 +0900

    [ modify ] 결제한 유저만 상품평 작성가능

--- src/apis/comments/comments.service.ts
+++ src/apis/comments/comments.service.ts
@@ -102,17 +102,17 @@ export class CommentsService {
     if (!user)
       throw new UnprocessableEntityException('구매자 정보를 입력해주세요');
 
-    //유저 다중으로 안해놓으면 상품이 바뀌어도 유저id로 한번밖에 글 작성을 못함
-    // const userDuplicate = await this.commentsRepository.findOne({
-    //   where: { user: { id: userId } },
-    // });
+    // 유저 다중으로 안해놓으면 상품이 바뀌어도 유저id로 한번밖에 글 작성을 못함
+    const userDuplicate = await this.commentsRepository.findOne({
+      where: { user: { id: userId } },
+    });
 
-    // if (userDuplicate) {
-    //   throw new UnprocessableEntityException(
-    //     user.name +
-    //       '님은 이미 구매평을 작성하셨습니다. 1인당 1개의 구매평만 작성 가능합니다',
-    //   );
-    // }
+    if (userDuplicate) {
+      throw new UnprocessableEntityException(
+        user.name +
+          '님은 이미 구매평을 작성하셨습니다. 1인당 1개의 구매평만 작성 가능합니다',
+      );
+    }
 
     const result = await this.commentsRepository.save({
       ...createCommentInput,
